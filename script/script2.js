99845cf7d9530a25b5ebf9b623222c867c7552f0
it <oleg-ivannikov@mail.ru>
2020-12-22 13:00:50 +0300

    Vue exercise 2

--- script/script2.js
+++ script/script2.js
@@ -5,3 +5,36 @@
 о пользователя сайта (например, имя, фамилия, 
 login, email, картинку аватара, и т.п.)
 */
+
+let app = Vue.createApp({
+    data(){
+        return {
+            name:   'user1Name',
+            surname: 'user1Surname',
+            login:  'user1Login',
+            email:  'user1@email.com',
+            img:    './image.user1.jpg'
+        }
+    },
+});
+
+app.component('userinfo',{
+    template: `<div>
+                    <span :textContent='name'>  </span>
+                    <span :textContent='surname'> </span>
+                    <span :textContent='login'> </span>
+                    <span :textContent='email'> </span>
+                    <img :src='img'>
+               </div>`,
+       data(){
+        return {
+            name:   'user1Name',
+            surname: 'user1Surname',
+            login:  'user1Login',
+            email:  'user1@email.com',
+            img:    '../image/user1.jpg'
+        }
+    },
+});
+
+app.mount('#userBoxInfo');
